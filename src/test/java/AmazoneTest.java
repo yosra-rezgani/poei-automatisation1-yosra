c4ceb6111bce04632eb4986485524155e5d699fc
yosra <yosra.rezgani6@gmail.com>
2023-04-03 14:59:00 +0200

    first commit

---
+++ src/test/java/AmazoneTest.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AmazoneTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    public void testAmazon() {
+        driver.get("https://www.amazon.fr");
+    }
+}
